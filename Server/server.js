495e992bd42ae132c2c3edd03d83a38265e54d8e
mateen987 <abdulmateen334@gmail.com>
2018-11-04 16:24:40 +0500

    now i add addon in app

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ var {user}=require('./Model/user');
 var {Todo}= require('./Model/todo');
 
 var app=express();
-const port= process.env.PORT || 3000;
+const PORT= process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.post('/todo',(req,res)=>{
 
@@ -51,8 +51,8 @@ app.get('/todo/:id',(req,res)=>{
 
 
 
-app.listen(port,()=>{
-console.log(`starting on port ${port}`);
+app.listen(PORT,()=>{
+console.log(`starting on port ${PORT}`);
 });
 
 module.exports={app};
