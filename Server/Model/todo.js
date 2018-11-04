a4323d766ed789cb6be6bff527be89c48044710b
mateen987 <abdulmateen334@gmail.com>
2018-11-04 18:57:03 +0500

    lecture

--- Server/Model/todo.js
+++ Server/Model/todo.js
@@ -5,7 +5,7 @@ var Todo = mongoose.model('todos',{
         type:String,
         required:true,
         minlength:1,
-        trim:true,
+        trim:true
     },
     completed:{
         type:Boolean,
