272b7362c79727b3bf2ef70f6016af8e5296da68
mateen987 <abdulmateen334@gmail.com>
2018-11-09 18:48:55 +0500

    add delete butn

--- Server/server.js
+++ Server/server.js
@@ -48,6 +48,23 @@ app.get('/todo/:id',(req,res)=>{
 
 })
 
+app.delete('/todo/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(400).send();
+    }
+Todo.findByIdAndRemove({id}).then((todo)=>{
+if(!todo){
+    return res.status(400).send();
+}
+res.send(todo);
+}).catch((e)=>{
+
+    res.status(400).send();
+})
+
+})
+
 app.listen(port, ()=>{
 console.log(`starting on port ${port}`);
 });
