8f69026f5ee8249cb4f11f2bc5d3f38e03dcd618
mateen987 <abdulmateen334@gmail.com>
2018-11-10 17:48:17 +0500

    add path/todos/:id

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,7 @@
+const _ = require('lodash');
 var express=require('express');
-var bodyParser=require('body-parser');
-
-var {ObjectID}= require('mongodb');
-
+const bodyParser=require('body-parser');
+const {ObjectID}= require('mongodb');
 var {mongoose} =require('./db/mongoose');
 var {user}=require('./Model/user');
 var {Todo}= require('./Model/todo');
@@ -62,6 +61,30 @@ res.send(todo);
 
     res.status(400).send();
 })
+
+})
+
+app.patch('/todo/:id',(req,res)=>{
+    var id = req.params.id;
+    var body =_.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    if( _.isBoolean(body.completed) && body.completed){
+        body.completedAt=new Date().getTime();
+    }else {
+      body.completed=false;
+      body.completedAt=null;   
+    }
+    Todo.findByIdAndUpdate(id ,{$set:body}, {new :true}).then((todo)=>{
+    if(!todo){
+        return res.status(404).send();
+    }
+
+    res.send({todo});
+    }).catch((e)=>{
+     res.status(400).send();
+    });
 
 })
 
