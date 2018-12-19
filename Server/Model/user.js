17779ce5649cce209117c79f06a8014d2e8d7e34
mateen987 <abdulmateen334@gmail.com>
2018-12-19 05:38:13 +0500

    Update code with configuration

--- Server/Model/user.js
+++ Server/Model/user.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 const validator=require('validator');
 const jwt=require('jsonwebtoken');
 const _=require('lodash');
+ const bcrypt=require('bcryptjs');
 
 var UserSchema=new mongoose.Schema({
     email:{
@@ -41,7 +42,7 @@ UserSchema.methods.toJSON=function(){
 UserSchema.methods.generateAuthToken=function(){ 
     var user=this;
     var access ='auth';
-    var token= jwt.sign({_id:user._id.toHexString(),access},'abc123').toString();
+    var token= jwt.sign({_id:user._id.toHexString(),access},process.env.JWT_SECRET).toString();
     user.tokens =user.tokens.concat([{access,token}]);
     return user.save().then(()=>{
           return token;
@@ -53,18 +54,64 @@ UserSchema.statics.findByToken=function(token){
     var User=this;
     var decoded ;
     try {
-         decoded = jwt.verify(token,'abc123');
+         decoded = jwt.verify(token,process.env.JWT_SECRET);
     } catch (e) {
       
     }
     return User.findOne({
       _id:decoded._id,
       'tokens.token':token,
-      'tokens.access':'auth',
-      
+      'tokens.access':'auth',  
     });
     
 }
+
+UserSchema.statics.findByCredentials= function (email,password){
+    var User=this;
+   return  User.findOne({email}).then((user)=>{
+       if(!user){
+          return Promise.reject();
+       }
+       return new Promise((resolve , reject)=>{
+           bcrypt.compare(password,user.password,(err, res)=>{
+               if(res){
+                   resolve(user);
+               }
+               else{
+                   reject();
+               }
+           })
+       
+       });
+   })
+}
+
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+      $pull: {
+        tokens: {token}
+      }
+    })
+  };
+
+UserSchema.pre('save',function(next){
+    var user=this;
+    if(user.isModified('password')){
+      bcrypt.genSalt(10,(req,salt)=>{
+      bcrypt.hash(user.password,salt,(res,hash)=>{
+       user.password=hash; 
+        next();
+      })
+      });
+    } 
+    else{
+         next();
+    }
+})
+
+
 
 var User = mongoose.model('User',UserSchema);
 module.exports={User}
--- Server/config/config.js
+++ Server/config/config.js
@@ -1,12 +1,23 @@
+
 var env= process.env.NODE_ENV || 'development';
+if(env==="development"|| env==="test"){
+var config = require('./config.json');
+ console.log(config);
 
-
-if(env==='development'){
-process.env.PORT=3000;
-process.env.MONGODB_URI = 'mongodb://localhost:27017/Todoapp';
-
-}else if(env==='test'){
-    process.env.PORT=3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoappTest';
-
+ var envconfig= config[env];
+ Object.keys(envconfig).forEach((key)=>{
+     process.env[key]=envconfig[key];
+ });
+ 
 }
+
+// if(env==='development'){
+// process.env.PORT=3000;
+// process.env.MONGODB_URI = 'mongodb://localhost:27017/Todoapp';
+
+// }else if(env==='test'){
+//     process.env.PORT=3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoappTest';
+
+// }
+
